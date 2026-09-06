b9d779509084bec654067c2f067d9f2f6ec9b9a4
xm2500-hash <xm2500@nyu.edu>
2026-09-06 19:37:34 -0400

    Add new quotes about creative coding
    Added new quotes to the quotes array, enhancing the variety of creative coding perspectives.

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,14 +2,22 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
-  { text: "process over results", source: "Sam" },
-  { text: "form over function", source: "Sean" },
-  { text: "iterative and experimental", source: "Scooby" },
-  { text: "a community of practice", source: "Seth" },
-  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "Creative coding is the practice of combining programming techniques with artistic expression in order to create aesthetically pleasing and optimized interactive digital experiences.", 
+   source: "Meadows School of the Arts" },
+  { text: "(Creative Coding) is a process, based on exploration, iteration, reflection and discovery, where code is used as the primary medium to create a wide range of media artifacts.", 
+   source: ".timrodenbroeker" },
+  { text: "Creative coding is a new interdisciplinary art form that bridges the gap between technologists and artists. ", 
+   source: "Gorilla Sun" },
+  { text: "Creative Coding is a loosely defined term used to describe a wide range of artistic practices that use computer code as a medium. ", 
+   source: "Creative Code Berlin" },
+  { text: "Creative coding is a practice where programming is used as a tool for art or design. ", 
+   source: "DigitalFUTURES" },
+  { text: "creative coding is the practice of using computer programming as a medium for artistic and personal expression.", 
+   source: "Diatom" },
+  { text: "a Creative Coding is a broad term that describes the process of using code, low-level technology, and computers as artistic and design tools. ", 
+   source: "Montana Media Art" },
+  { text: "Creative coding is an excellent introduction to programming and graphics, and complementary to learning real-time 3D engines.", 
+   source: "The Guidebook" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
