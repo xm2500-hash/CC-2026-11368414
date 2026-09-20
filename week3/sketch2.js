908369731d4560cc906a2b7a38e4f2aa0287c9a1
xm2500-hash <xm2500@nyu.edu>
2026-09-20 14:17:38 -0400

    Modify canvas size and implement interactive circle
    Updated canvas size and added dynamic circle size and color based on mouse position.

--- week3/sketch2.js
+++ week3/sketch2.js
@@ -1,10 +1,33 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  createCanvas(800, 800);
+  noStroke();
 }
 
 function draw() {
-  
+  background(240);
+
+  let circleSize;
+//如果鼠标在Y上比400小，那动一下Y+100
+  if (mouseY < 400) {
+    circleSize = mouseY + 100;
+    //如果反之，
+    //900是因为我希望最小的圆也有100大小
+  } else {
+    circleSize = 900 - mouseY;
+  }
+
+  let colorChange;
+//如果鼠标在 左边，把鼠标当前在 X 轴上的位置除以 4
+  if (mouseX < 400) {
+    colorChange = mouseX / 4;
+  } else {
+    colorChange = (800 - mouseX) / 4;
+  }
+
+//粉色和紫色，
+  fill(180 + colorChange, 100 + colorChange, 220);
+
+  circle(400, 400, circleSize);
 }
