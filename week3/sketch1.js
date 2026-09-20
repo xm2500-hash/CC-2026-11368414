24b7e85796134c8b61720cf0393446a2709eba72
xm2500-hash <xm2500@nyu.edu>
2026-09-20 13:46:45 -0400

    Add sketch1.js file

---
+++ week3/sketch1.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(240);
+ noStroke();
+  //只要X还比画布小，就一直加
+  for (let x = 30; x < width; x += 60) {
+    for (let y = 20; y < height; y += 60) {
+//圆的大小取决于它现在的 y 位置。
+      let size = y / 8;
+//从左到右的变化
+      fill(mouseX / 4, mouseY / 4, 100);
+
+      circle(x, y, size);
+    }
+  }
+}
