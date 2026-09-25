a5ff16912fb9ae12249742ab54e0643af8b79a50
xm2500-hash <xm2500@nyu.edu>
2026-09-25 13:48:09 -0400

    Implement SVG saving feature in sketch.js
    Added functionality to save SVG of the circle pattern when 's' key is pressed.

--- week 4/sketch.js
+++ week 4/sketch.js
@@ -1,3 +1,5 @@
+let saveSvg = false;
+
 function setup() {
   createCanvas(576, 384);
 }
@@ -7,17 +9,32 @@ function draw() {
   noFill();
   randomSeed(10);
 
+  if (saveSvg) {
+    beginRecordSvg("circlePattern.svg");
+  }
+
   let maxSize = map(mouseX, 0, width, 50, 80);
 
   for (let x = 50; x < width; x += 80) {
-  for (let y = 40; y < height; y += 60) {
+    for (let y = 40; y < height; y += 60) {
 
-  let size = map(y, 40, height, 30, maxSize);
-//随机颜色
-  stroke(random(255), random(255), random(255));
+      let size = map(y, 40, height, 30, maxSize);
+
+      stroke(0);
 
       circle(x, y, size);
       circle(x, y, size * 0.6);
     }
+  }
+
+  if (saveSvg) {
+    endRecordSvg();
+    saveSvg = false;
+  }
+}
+
+function keyPressed() {
+  if (key == 's') {
+    saveSvg = true;
   }
 }
