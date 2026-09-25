ec3f350431e0f20b322ab4f296af988abe9fc64d
xm2500-hash <xm2500@nyu.edu>
2026-09-25 13:31:57 -0400

    Modify canvas size and implement circle drawing
    Change canvas size and add dynamic circle drawing.

--- week 4/sketch.js
+++ week 4/sketch.js
@@ -1,10 +1,23 @@
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  
+  createCanvas(576, 384);
 }
 
 function draw() {
-  
+  background(255);
+  noFill();
+  randomSeed(10);
+
+  let maxSize = map(mouseX, 0, width, 50, 80);
+
+  for (let x = 50; x < width; x += 80) {
+  for (let y = 40; y < height; y += 60) {
+
+  let size = map(y, 40, height, 30, maxSize);
+//随机颜色
+  stroke(random(255), random(255), random(255));
+
+      circle(x, y, size);
+      circle(x, y, size * 0.6);
+    }
+  }
 }
